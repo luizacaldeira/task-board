cf44e05ed9213ff7ddca9fd93551f006f4579640
Luiza Caldeira Daniel <luiza.caldeira@estudante.ufjf.br>
2026-02-04 02:32:40 -0300

    feat: update TaskListSection to use TaskCard component and improve styling

--- taskBoard/src/app/components/task-list-section/task-list-section.ts
+++ taskBoard/src/app/components/task-list-section/task-list-section.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { LucideAngularModule } from 'lucide-angular';
+import { TaskCard } from '../task-card/task-card';
 
 @Component({
   selector: 'app-task-list-section',
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, TaskCard],
   templateUrl: './task-list-section.html',
   styleUrl: './task-list-section.css',
 })
