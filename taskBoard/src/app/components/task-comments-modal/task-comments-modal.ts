4d39b5265548750e0bfb2063644f49131e317b7c
Luiza Caldeira Daniel <luiza.caldeira@estudante.ufjf.br>
2026-02-04 12:36:29 -0300

    feat: implement TaskCommentsModal with comments section and input functionality

--- taskBoard/src/app/components/task-comments-modal/task-comments-modal.ts
+++ taskBoard/src/app/components/task-comments-modal/task-comments-modal.ts
@@ -1,11 +1,14 @@
+import { L } from '@angular/cdk/keycodes';
 import { Component } from '@angular/core';
+import { LucideAngularModule, X, SquareStar  } from 'lucide-angular';
 
 @Component({
   selector: 'app-task-comments-modal',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './task-comments-modal.html',
   styleUrl: './task-comments-modal.css',
 })
 export class TaskCommentsModal {
-
+  readonly X = X;
+  readonly SquareStar  = SquareStar ;
 }
