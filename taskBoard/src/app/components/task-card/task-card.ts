c75fec0fb225b4fe1baf75e4816bdd93037b19cc
Luiza Caldeira Daniel <luiza.caldeira@estudante.ufjf.br>
2026-02-04 15:45:54 -0300

    feat: add dropdown menu for task options with edit and delete functionality

--- taskBoard/src/app/components/task-card/task-card.ts
+++ taskBoard/src/app/components/task-card/task-card.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import {LucideAngularModule, Trash, MessageCircle } from 'lucide-angular';
+import { Component, ElementRef, HostListener } from '@angular/core';
+import {LucideAngularModule, Trash, MessageCircle, Pen, EllipsisVertical, X } from 'lucide-angular';
 
 @Component({
   selector: 'app-task-card',
@@ -7,7 +7,29 @@ import {LucideAngularModule, Trash, MessageCircle } from 'lucide-angular';
   templateUrl: './task-card.html',
   styleUrl: './task-card.css',
 })
+
 export class TaskCard {
+  // Icons
   readonly Trash = Trash;
   readonly MessageCircle  = MessageCircle ;
+  readonly Pen = Pen;
+  readonly EllipsisVertical = EllipsisVertical;
+  readonly X = X;
+  
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+  closeMenu() {
+    this.menuOpen = false;
+  }
+
+  constructor(private elementRef: ElementRef) {}
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      this.closeMenu();
+    }
+  }
 }
