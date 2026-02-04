4628c08a982b300b966a4b739646ff4d68d915ac
Luiza Caldeira Daniel <luiza.caldeira@estudante.ufjf.br>
2026-02-04 18:31:08 -0300

    feat: add utility function to generate unique IDs

---
+++ taskBoard/src/app/utils/generate-unique-id.ts
@@ -0,0 +1,5 @@
+export const GenerateUniqueId = (): string => {
+   const timestamp = new Date().getTime().toString(36);
+   const randomPart = Math.random().toString(36).substring(2, 9);
+   return `${timestamp}-${randomPart}`;
+};
