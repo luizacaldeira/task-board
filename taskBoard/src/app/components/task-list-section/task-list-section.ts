69f66ca203ab30279d6e2d2df90ed11ad20eb4ed
Luiza Caldeira Daniel <luiza.caldeira@estudante.ufjf.br>
2026-02-04 02:10:52 -0300

    feat: add TaskListSection component with template, styles, and tests

---
+++ taskBoard/src/app/components/task-list-section/task-list-section.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { LucideAngularModule } from 'lucide-angular';
+
+@Component({
+  selector: 'app-task-list-section',
+  imports: [LucideAngularModule],
+  templateUrl: './task-list-section.html',
+  styleUrl: './task-list-section.css',
+})
+export class TaskListSection {
+
+}
