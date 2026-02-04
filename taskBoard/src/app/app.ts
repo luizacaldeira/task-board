dce76f1646e8d6e1e9c21e27559a8dfe662ae9b2
Luiza Caldeira Daniel <luiza.caldeira@estudante.ufjf.br>
2026-02-04 01:46:53 -0300

    feat: update app component to include Header and MainContent components

--- taskBoard/src/app/app.ts
+++ taskBoard/src/app/app.ts
@@ -1,9 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { LucideAngularModule, CircleCheckBig } from 'lucide-angular';
+import { MainContent } from './components/main-content/main-content';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: 
+  [
+    Header,
+    MainContent,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
