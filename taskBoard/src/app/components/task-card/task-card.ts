1cf0177a3f3ac7ade56216aca597718de5fb4bd6
Luiza Caldeira Daniel <luiza.caldeira@estudante.ufjf.br>
2026-02-05 23:48:15 -0300

    feat: enhance task card component with dynamic task data and delete functionality

--- taskBoard/src/app/components/task-card/task-card.ts
+++ taskBoard/src/app/components/task-card/task-card.ts
@@ -1,14 +1,18 @@
-import { Component, ElementRef, HostListener, inject } from '@angular/core';
+import { Component, ElementRef, HostListener, inject, Input } from '@angular/core';
 import { LucideAngularModule, Trash, MessageCircle, Pen, EllipsisVertical } from 'lucide-angular';
 import { ModalControllerService } from '../../services/modal-controller-service';
+import { ITask } from '../../interfaces/task-interface';
+import { TaskService } from '../../services/task-service';
+import { SlicePipe } from '@angular/common';
 
 @Component({
   selector: 'app-task-card',
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, SlicePipe],
   templateUrl: './task-card.html',
   styleUrl: './task-card.css',
 })
 export class TaskCard {
+
   // Icons
   readonly Trash = Trash;
   readonly MessageCircle = MessageCircle;
@@ -17,17 +21,23 @@ export class TaskCard {
 
   // Services
   private readonly elementRef = inject(ElementRef);
-  private readonly modalService = inject(ModalControllerService);
+  private readonly _modalService = inject(ModalControllerService);
+  private readonly _taskService = inject(TaskService);
 
   // State
   menuOpen = false;
 
-  // Host Listeners
+  @Input({ required: true }) task!: ITask;
+
   @HostListener('document:click', ['$event'])
   onDocumentClick(event: MouseEvent) {
     if (!this.elementRef.nativeElement.contains(event.target)) {
       this.closeMenu();
     }
+  }
+
+  deleteTask() {
+    this._taskService.deleteTask(this.task.id, this.task.status);
   }
 
   // Menu Methods
@@ -42,16 +52,29 @@ export class TaskCard {
   // Modal Methods
   openEditModal() {
     this.closeMenu();
-    const dialogRef = this.modalService.openEditTaskModal({
-      name: "Task name",
-      description: "Task description",
+    const dialogRef = this._modalService.openEditTaskModal({
+      name: this.task.name,
+      description: this.task.description,
     });
     dialogRef.closed.subscribe((taskForm)=>{
       console.log('Task Edited:', taskForm);
+      if(taskForm){
+        this._taskService.updateTaskInfos(
+          this.task.id,
+          this.task.status,
+          taskForm.name,
+          taskForm.description,
+        )
+      }
     })
   }
 
   openCommentsModal() {
-    this.modalService.openTaskCommentsModal();
+    const dialogRef = this._modalService.openTaskCommentsModal(this.task);
+    dialogRef.closed.subscribe((taskCommentsChanged) => {
+      if(taskCommentsChanged){
+        this._taskService.updateTaskComments(this.task.id, this.task.status, this.task.comments);
+      }
+    });
   }
 }
