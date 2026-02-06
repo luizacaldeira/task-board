b82a1e5542160c176269de4be1dcb23a05791296
Luiza Caldeira Daniel <luiza.caldeira@estudante.ufjf.br>
2026-02-05 23:47:53 -0300

    refactor: remove unused imports from app component

--- taskBoard/src/app/app.ts
+++ taskBoard/src/app/app.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { Header } from './components/header/header';
 import { MainContent } from './components/main-content/main-content';
-import { TaskFormModal } from './components/task-form-modal/task-form-modal';
-import { TaskCommentsModal } from './components/task-comments-modal/task-comments-modal';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +8,6 @@ import { TaskCommentsModal } from './components/task-comments-modal/task-comment
   [
     Header,
     MainContent,
-    TaskFormModal,
-    TaskCommentsModal
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
