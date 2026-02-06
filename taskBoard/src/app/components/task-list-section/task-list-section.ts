13c0af9d80ae1e2311f5f88210b4c281e2e9237c
Luiza Caldeira Daniel <luiza.caldeira@estudante.ufjf.br>
2026-02-05 23:49:11 -0300

    feat: implement drag-and-drop functionality for task management with status updates

--- taskBoard/src/app/components/task-list-section/task-list-section.ts
+++ taskBoard/src/app/components/task-list-section/task-list-section.ts
@@ -1,13 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { LucideAngularModule } from 'lucide-angular';
 import { TaskCard } from '../task-card/task-card';
+import { TaskService } from '../../services/task-service';
+import {
+  CdkDrag,
+  CdkDragDrop,
+  CdkDropList,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
+import { ITask } from '../../interfaces/task-interface';
+import { AsyncPipe, JsonPipe } from '@angular/common';
+import { TaskStatus } from '../../../types/task-status';
+import { TaskStatusEnum } from '../../enums/task-status-enum';
 
 @Component({
   selector: 'app-task-list-section',
-  imports: [LucideAngularModule, TaskCard],
+  imports: [LucideAngularModule, TaskCard, CdkDropList, CdkDrag, AsyncPipe, JsonPipe],
   templateUrl: './task-list-section.html',
   styleUrl: './task-list-section.css',
 })
-export class TaskListSection {
 
+export class TaskListSection {
+  readonly _taskService = inject(TaskService);
+
+  onCardDrop(event: CdkDragDrop<ITask[]>){
+    this.moveCardToColumn(event);
+    const taskId = event.item.data.id;
+    const taskCurrentStatus = event.item.data.status;
+    const droppedColumn = event.container.id;
+
+    this.updateTaskStatus(taskId, taskCurrentStatus, droppedColumn);
+  }
+
+  private updateTaskStatus(taskId: string, taskCurrentStatus: TaskStatus, droppedColumn: string) {
+    let taskNextStatus: TaskStatus;
+    switch (droppedColumn) {
+      case 'to-do-column':
+        taskNextStatus = TaskStatusEnum.TODO;
+        break;
+      case 'in-progress-column':
+        taskNextStatus = TaskStatusEnum.INPROGRESS;
+        break;
+      case 'done-column':
+        taskNextStatus = TaskStatusEnum.DONE;
+        break;
+      default:
+        throw new Error("Invalid column.");
+    }
+    this._taskService.updateTaskStatus(taskId, taskCurrentStatus, taskNextStatus);
+  }
+
+  private moveCardToColumn(event: CdkDragDrop<ITask[]>) {
+    console.log(event);
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
 }
