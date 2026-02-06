0a95913f56fdfebbd1b4f9260f2c26acc7ba1e1d
Luiza Caldeira Daniel <luiza.caldeira@estudante.ufjf.br>
2026-02-05 23:49:22 -0300

    feat: integrate task service for adding new tasks in welcome section

--- taskBoard/src/app/components/welcome-section/welcome-section.ts
+++ taskBoard/src/app/components/welcome-section/welcome-section.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { LucideAngularModule, CirclePlus } from 'lucide-angular';
 import { ModalControllerService } from '../../services/modal-controller-service';
+import { TaskService } from '../../services/task-service';
 
 @Component({
   selector: 'app-welcome-section',
@@ -12,11 +13,16 @@ export class WelcomeSection {
   readonly CirclePlus = CirclePlus;
 
   private readonly _modalControllerService = inject(ModalControllerService);
+  private readonly _taskService = inject(TaskService);
 
   openCreateModal (){
     const dialogRef = this._modalControllerService.openNewTaskModal();
+
     dialogRef.closed.subscribe((taskForm)=> {
       console.log('New Task Created:', taskForm);
+      if(taskForm) {
+        this._taskService.addTask(taskForm);
+      }
     })
   }
 }
