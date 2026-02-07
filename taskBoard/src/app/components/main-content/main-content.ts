8b6420fdf21cc71040b7e7c656e4ce7a6fb444b7
Luiza Caldeira Daniel <luiza.caldeira@estudante.ufjf.br>
2026-02-06 22:41:42 -0300

    feat: add task creation button and integrate modal for new tasks

--- taskBoard/src/app/components/main-content/main-content.ts
+++ taskBoard/src/app/components/main-content/main-content.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { WelcomeSection } from '../welcome-section/welcome-section';
 import { TaskListSection } from '../task-list-section/task-list-section';
+import { CirclePlus, LucideAngularModule } from 'lucide-angular';
+import { ModalControllerService } from '../../services/modal-controller-service';
+import { TaskService } from '../../services/task-service';
+import { ThemeService } from '../../services/theme-service';
 
 @Component({
   selector: 'app-main-content',
-  imports: [WelcomeSection, TaskListSection],
+  imports: [WelcomeSection, TaskListSection, LucideAngularModule],
   templateUrl: './main-content.html',
   styleUrl: './main-content.css',
 })
 export class MainContent {
+  readonly CirclePlus = CirclePlus;
 
+  private readonly _modalControllerService = inject(ModalControllerService);
+  private readonly _taskService = inject(TaskService);
+  readonly _themeService = inject(ThemeService);
+
+  openCreateModal (){
+    const dialogRef = this._modalControllerService.openNewTaskModal();
+
+    dialogRef.closed.subscribe((taskForm)=> {
+      console.log('New Task Created:', taskForm);
+      if(taskForm) {
+        this._taskService.addTask(taskForm);
+      }
+    })
+  }
 }
